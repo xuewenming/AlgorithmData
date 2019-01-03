3a0eed54a7b6a4239c02eb606a33be19f872b70a
Sam <xuewenmingwork@163.com>
2019-01-04 00:11:08 +0800

    Initial commit

---
+++ 05_array/src/com/sam/array/Array.java
@@ -0,0 +1,19 @@
+package com.sam.array;
+
+/**
+ * 数组
+ * @author Sam
+ * @date 2019/1/3
+ * @time 23:55
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+
+
+
+
+
+    }
+}
